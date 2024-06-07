a878c2c355733a2fe350174b8251ce0f022a99c7
Lev Snegov <leffsnegov@yandex.ru>
2024-06-07 16:57:05 +0300

    папа сделал всё, согласно чек-листу и чату гпт,lol

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
+const cardsList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-
+function createCardElement(item) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');	
+  cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card__title').textContent = item.name;
+  
+  deleteButton.addEventListener('click', eraseButton);
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function eraseButton (e) {
+	e.target.closest('.places__item').remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item) {
+  const initialElement = createCardElement(item);
+  cardsList.append(initialElement);
+});
